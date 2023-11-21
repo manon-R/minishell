9dce80f76c90fcc099801164a85b1c61a593b49c
Manon LE BRAS <mle-bras@made-f0Br7s9.clusters.42paris.fr>
2023-11-21 17:29:19 +0100

    Start working on complete_redir_node in parser.c

--- parser.c
+++ parser.c
@@ -1,18 +1,28 @@
 #include "minishell.h"
 
-// int	complete_redir_node(t_node **node_tab, int *size)
-// {
-// 	int	i;
+char	*extract_str()
+{
+	
+}
 
-// 	i = 0;
-// 	while (i < *size)
-// 	{
-// 		if (is_redir_node((*node_tab)[i]) == SUCCESS)
-// 		{
+int	complete_redir_node(t_node **node_tab, int *size)
+{
+	int	i;
 
-// 		}
-// 	}
-// }
+	i = 0;
+	while (i < *size)
+	{
+		if (is_redir_node((*node_tab)[i]) == SUCCESS)
+		{
+			printf("HERE post redir = %s\n", (*node_tab)[i + 1].token);
+			free((*node_tab)[i].token);
+			(*node_tab)[i].token = extract_str((*node_tab)[i + 1].token);
+			//remplacer token par (*node_tab)[i + 1].token premier str et le reste sera une/des option
+		}
+		i++;
+	}
+	return (SUCCESS);
+}
 
 void	parser(t_node **node_tab, int size, t_var_env **env_list)
 {
@@ -21,8 +31,8 @@ void	parser(t_node **node_tab, int size, t_var_env **env_list)
 	if (check_error_node(node_tab, size) == FAIL)
 		return ;
 	check_env_var(node_tab, size, env_list);
-	// if (complete_redir_node(node_tab, &size) == FAIL)
-	// 	return ;
+	if (complete_redir_node(node_tab, &size) == FAIL)
+		return ;
 }
 
 /* TO DO 
